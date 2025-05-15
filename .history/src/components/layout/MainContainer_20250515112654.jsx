92c2b16cf380edad70c8f2acf7c6858fc7484cbb
Gia266 <154775233+Gia266@users.noreply.github.com>
2025-05-15 11:31:37 +0900

    header

--- .history/src/components/layout/MainContainer_20250515112654.jsx
+++ .history/src/components/layout/MainContainer_20250515112654.jsx
@@ -4,7 +4,7 @@ const MainContainer = ({ children }) => {
   return (
     <div
       className="w-full min-w-full h-[100vh]
-    flex justify-center p-0
+    flex  p-0
     "
     >
       <div className="w-full max-w-[48rem] px-2 sm:px-4">{children}</div>
